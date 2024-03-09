ca25a3a05619accde3aaf359c86eac498b2d95e7
Lili Gallego <lgallego@unal.edu.co>
2024-03-08 20:27:51 -0500

    intento_4

--- clean_data.py
+++ clean_data.py
@@ -36,16 +36,21 @@ def create_key(df, n):
     # - Convierta el texto a una lista de n-gramas
     df['key']=df['key'].map(lambda x:[x[t:t+n-1] for t in range(len(x))])
 
-    #
+    
     # - Ordene la lista de n-gramas y remueve duplicados
-    df['key']=df['key'].str.join('')
+    
+    df["key"] = df["key"].apply(lambda x: sorted(set(x)))
 
     #
     # - Convierta la lista de ngramas a una cadena
+    df['key']=df['key'].str.join('')
+    
 
     ## ------------------------------------------------------
 
     return df
+    
+
 
 
 def generate_cleaned_column(df):
